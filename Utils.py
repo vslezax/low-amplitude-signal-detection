a5c3e61fc9b188089af76ce3b58cbc84947dfe47
vslezax <zhurkovjaroslav@gmail.com>
2025-02-08 22:27:52 +0300

    SNR implemented

--- Utils.py
+++ Utils.py
@@ -2,6 +2,26 @@ import numpy as np
 from skimage.filters import threshold_otsu
 import matplotlib.pyplot as plt
 
+def SNR_to_points_count_iterations(SNR, total_points_count):
+    N_noise = total_points_count // (1 + SNR)
+    N_signal = SNR * np.sqrt(N_noise)
+
+    while abs(N_signal + N_noise - total_points_count) > 1:
+        N_noise = total_points_count - N_signal
+        N_signal = SNR * np.sqrt(N_noise)
+
+    N_signal = int(N_signal)
+    N_noise = int(N_noise)
+
+    return N_signal, N_noise
+
+def SNR_to_points_count_quadratic(SNR, total_points_count):
+    N_signal = (np.sqrt(SNR ** 4 + 4 * SNR ** 2 * total_points_count) - SNR ** 2) / 2
+    N_signal = int(N_signal)
+    N_noise = total_points_count - N_signal
+
+    return N_signal, N_noise
+
 def show_reference_signal_left(s_p_x, s_p_y, s_n_x, s_n_y):
     plt.subplot(1, 2, 1)
     plt.scatter(s_p_x, s_p_y, color='red')
--- master.py
+++ master.py
@@ -1,3 +1,4 @@
+from Utils import SNR_to_points_count_iterations, SNR_to_points_count_quadratic
 from signal_generator import generate_signal_by_counts, show_signal
 
 from Methods.DBSCAN_method import DBSCAN_method
@@ -11,7 +12,8 @@ from Methods.GMM_method import GMM_method
 
 
 def main():
-    s_x, s_y, s_p_x, s_p_y, n_p_x, n_p_y = generate_signal_by_counts(10, 20, .01, 0.2)
+    S_count, N_count = SNR_to_points_count_quadratic(3, 30)
+    s_x, s_y, s_p_x, s_p_y, n_p_x, n_p_y = generate_signal_by_counts(S_count, N_count, .01, 0.5)
     show_signal(s_p_x, s_p_y, n_p_x, n_p_y)
     # show_combined_signal(s_x, s_y)
 
